60ac8a0789486bc02af4e85f7b3e1c4eb952fdba
Shariar <shariarnafis86@gmail.com>
2024-01-16 11:16:24 +0600

    set up navbar

--- src/app/page.js
+++ src/app/page.js
@@ -3,9 +3,10 @@
 const HomePage = () => {
   return (
     <div>
+      <div className="test"> kasdf;lkdsj;l</div>
+      <div className="test"> dlksdj;lkajd;lksa</div>
       this is home.
       we will create our dream here
-      
     </div>
   );
 };
