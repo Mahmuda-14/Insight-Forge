cfebaf6b30ec75c0e5ca343539a8ee1322504204
Shariar <shariarnafis86@gmail.com>
2024-01-16 18:06:20 +0600

    set a logo in navbar

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,6 @@
 const HomePage = () => {
   return (
     <div>
-      <div className="test"> kasdf;lkdsj;l</div>
-      <div className="test"> dlksdj;lkajd;lksa</div>
       this is home.
       we will create our dream here
     </div>
