60a0cbfe1a6f79d1b487dc2eafdb3551368ae957
Mahmuda-14 <mahmudaiiuc14@gmail.com>
2024-01-17 19:23:17 +0600

    updated

--- src/app/page.js
+++ src/app/page.js
@@ -1,51 +1,25 @@
 
 
-<<<<<<< HEAD
+// import Review from "@/components/Review/Review";
+// import Banner from "@/components/banner/page";
+
 import Review from "@/Components/Review/Review";
 import All from "@/Components/Social/All";
 import Banner from "@/Components/banner/page";
 import PostSection from "@/Components/ui/PostSection/PostSection";
-=======
-import Review from "@/components/Review/Review";
-<<<<<<< HEAD
-import Count from "@/components/shared/count/Count";
-import Logo from "@/components/shared/logo/Logo";
 
-=======
-import Banner from "@/components/banner/page";
->>>>>>> 552f37ca4da6fd23a9a3d2a53809b578cccc47c2
->>>>>>> 0151455e7ca5e50846915427fbdf7c9e6dbad864
-
+// import PostSection from "@/components/ui/PostSection/PostSection";
 
 const HomePage = () => {
   return (
     <div>
-<<<<<<< HEAD
-      this is home.
-      we will create our dream here
-=======
-<<<<<<< HEAD
-      this is home.
-      we will create our dream here
-     
-      <PostSection />
-      <Review></Review>
-      <Count></Count>
-      <Logo></Logo>
-      <h2>ok....................</h2>
-=======
->>>>>>> 0151455e7ca5e50846915427fbdf7c9e6dbad864
       <Banner></Banner>
       <PostSection />
-      <Review></Review>     
-      <All></All>  
-      <PostSection />
       <Review></Review>
+      <All></All>
       
->>>>>>> 552f37ca4da6fd23a9a3d2a53809b578cccc47c2
     </div>
   );
 };
 
 export default HomePage;
-
