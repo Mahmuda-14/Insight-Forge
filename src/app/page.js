71627fa968a7c75dee29c56be4fe3a55c5f1f33b
muna2411 <nurnahar.muna.2411@gmail.com>
2024-01-17 18:13:56 +0600

    count and logo

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,7 @@
 import Review from "@/components/Review/Review";
+import Count from "@/components/shared/count/Count";
+import Logo from "@/components/shared/logo/Logo";
+
 
 import PostSection from "@/components/ui/PostSection/PostSection";
 
@@ -7,8 +10,11 @@ const HomePage = () => {
     <div>
       this is home.
       we will create our dream here
-      <Review></Review>
+     
       <PostSection />
+      <Review></Review>
+       <Count></Count>
+       <Logo></Logo>
     </div>
   );
 };
