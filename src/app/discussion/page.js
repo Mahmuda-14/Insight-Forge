f4953f366a810e621a49bebda8af57a0841d8c7c
Shariar <shariarnafis86@gmail.com>
2024-01-21 21:06:54 +0600

    question modal is complete

--- src/app/discussion/page.js
+++ src/app/discussion/page.js
@@ -1,12 +1,46 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 "use client"
-import { Box, Button, Fab, Grid, Stack, Typography } from "@mui/material";
+import Backdrop from '@mui/material/Backdrop';
+import { Box, Button, Fab, Fade, Grid, InputLabel, MenuItem, Modal, Select, Stack, TextField, Typography } from "@mui/material";
 import './discus.css'
 import { QuestionAnswer } from "@mui/icons-material";
 import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt';
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
 
 const page = () => {
+
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+    
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [age, setAge] = React.useState('');
+
+    const handleChange = (event) => {
+        setAge(event.target.value);
+    };
+
+    const handlePost = e =>{
+        e.preventDefault()
+        const from = e.target
+        const title = from.title.value
+        const description = from.description.value
+        const category = from.category.value
+        console.log(title, description, category)
+    }
 
 
     // eslint-disable-next-line react-hooks/rules-of-hooks
@@ -26,6 +60,7 @@ const page = () => {
 
     return (
         <Box className="discussion">
+            {/*header part*/}
             <Stack className="discusHeader"
                 direction="row"
                 justifyContent="space-between"
@@ -33,10 +68,63 @@ const page = () => {
                 spacing={1}
             >
                 <Typography className="topQus" variant="h4">Top Questions</Typography>
-                {/* <Button variant="outlined">AskQuestion</Button> */}
-                <button className="askBtn" variant="outlined">Ask Question</button>
+                <div>
+                    <Button className="askBtn" variant="outlined" onClick={handleOpen}>Ask Question</Button>
+                    <Modal
+                        aria-labelledby="transition-modal-title"
+                        aria-describedby="transition-modal-description"
+                        open={open}
+                        onClose={handleClose}
+                        closeAfterTransition
+                        slots={{ backdrop: Backdrop }}
+                        slotProps={{
+                            backdrop: {
+                                timeout: 500,
+                            },
+                        }}
+                    >
+                        <Fade in={open}>
+                            <Box sx={style}>
+                                <form onSubmit={handlePost} className='from'>
+                                    <TextField name='title'
+                                        className='input'
+                                        id="outlined-multiline-flexible"
+                                        label="Title"
+                                        multiline
+                                        maxRows={4}
+                                    />
+                                    <TextField name='description'
+                                        className='input'
+                                        id="outlined-multiline-static"
+                                        label="Description"
+                                        multiline
+                                        rows={4}
+                                    />
+                                    <InputLabel id="demo-select-small-label">Age</InputLabel>
+                                    <Select className='input' name='category'
+                                        labelId="demo-simple-select-autowidth-label"
+                                        id="demo-simple-select-autowidth"
+                                        autoWidth
+                                        label="Age"
+                                        onChange={handleChange}
+                                    >
+                                        <MenuItem value="">
+                                            <em>None</em>
+                                        </MenuItem>
+                                        <MenuItem value={10}>Ten</MenuItem>
+                                        <MenuItem value={20}>Twenty</MenuItem>
+                                        <MenuItem value={30}>Thirty</MenuItem>
+                                    </Select>
+                                    <Button type='submit' className="qusPost" variant="outlined">Post Question</Button>
+                                </form>
+                            </Box>
+                        </Fade>
+                    </Modal>
+                </div>
             </Stack>
             <Box className="borderBot"></Box>
+
+            {/* question part */}
 
             <Grid container className="discusContainer" spacing={2}>
                 <Grid lg={3} >content</Grid>
@@ -47,8 +135,8 @@ const page = () => {
                             <h4>How do I break a string into words and track the index of is a each word (within the original string)?</h4>
                             <p>50 Answers · 10 hours ago</p>
                             <div className="btnIcon">
-                            <Button> <QuestionAnswer /> Answer</Button>
-                            <Button className="like"><ThumbUpOffAltIcon /></Button>
+                                <Button> <QuestionAnswer /> Answer</Button>
+                                <Button className="like"><ThumbUpOffAltIcon /></Button>
                             </div>
                             <div className="qusDivider"></div>
                         </div>)
