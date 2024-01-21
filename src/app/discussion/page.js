1b78fc828b8a440d331f1a2ecee9a02bf4d39bab
Shariar <shariarnafis86@gmail.com>
2024-01-21 18:34:10 +0600

    style question section

--- src/app/discussion/page.js
+++ src/app/discussion/page.js
@@ -1,6 +1,29 @@
-import { Box, Button, Stack, Typography } from "@mui/material";
+/* eslint-disable react-hooks/exhaustive-deps */
+"use client"
+import { Box, Button, Fab, Grid, Stack, Typography } from "@mui/material";
+import './discus.css'
+import { QuestionAnswer } from "@mui/icons-material";
+import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt';
+import { useEffect, useState } from "react";
 
 const page = () => {
+
+
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [questions, setQuestions] = useState([]);
+
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useEffect(() => {
+        fetch("/blog.json")
+            .then((res) => res.json())
+            .then((data) => {
+                setQuestions(data);
+            });
+    }, []);
+
+    console.log(questions)
+
+
     return (
         <Box className="discussion">
             <Stack className="discusHeader"
@@ -13,6 +36,25 @@ const page = () => {
                 {/* <Button variant="outlined">AskQuestion</Button> */}
                 <button className="askBtn" variant="outlined">Ask Question</button>
             </Stack>
+            <Box className="borderBot"></Box>
+
+            <Grid container className="discusContainer" spacing={2}>
+                <Grid lg={3} >content</Grid>
+                <Grid lg={6} className="qusContainer">
+                    {
+                        questions?.map(question => <div key={question?._id}>
+
+                            <h4>How do I break a string into words and track the index of is a each word (within the original string)?</h4>
+                            <p>50 Answers · 10 hours ago</p>
+                            <button> <QuestionAnswer /> Answer</button>
+                            <Button><ThumbUpOffAltIcon /></Button>
+                            <div className="qusDivider"></div>
+                        </div>)
+                    }
+                </Grid>
+                <Grid lg={3} >content</Grid>
+            </Grid>
+
         </Box>
     );
 };
