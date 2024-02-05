9915bba938b12275bcda1e55c8cfd97f0f77d49b
Mahmuda-14 <mahmudaiiuc14@gmail.com>
2024-02-05 21:02:45 +0600

    modified

--- src/app/page.js
+++ src/app/page.js
@@ -6,14 +6,9 @@ import Chatbot from "@/components/chatbot/chatbot";
 import DrawerAppBar from "@/components/shared/Navbar/Navbar";
 
 import Footer from "@/components/shared/footer/Footer";
-<<<<<<< HEAD
 import Logo from "@/components/shared/logo/Logo";
 import { Box, Container, Divider } from "@mui/material";
 import Discuss from "./discussion/Discuss";
-=======
-
-import { Box, Container } from "@mui/material";
->>>>>>> 89cbd3f5e3e364511551da224db7197850f811a7
 
 
 // import PostSection from "@/components/ui/PostSection/PostSection";
@@ -24,7 +19,6 @@ const HomePage = () => {
       <DrawerAppBar></DrawerAppBar>
       <Divider sx={{ backgroundColor: 'white',paddingTop:'45px' }} />
       <Banner></Banner>
-<<<<<<< HEAD
       <Discuss></Discuss>
       {/* <Chatbot></Chatbot>
       <Review></Review>
@@ -32,10 +26,6 @@ const HomePage = () => {
       <Count></Count>
       <Logo></Logo>
       <Footer></Footer> */}
-=======
-      <Chatbot></Chatbot>
-      <Footer></Footer>
->>>>>>> 89cbd3f5e3e364511551da224db7197850f811a7
     </Box>
   );
 };
