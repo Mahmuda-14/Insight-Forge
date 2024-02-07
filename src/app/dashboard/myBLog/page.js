abb40ad72c3f8fe6b04819fb3e4fa20203da7e17
Shariar2006 <shariarnafis86@gmail.com>
2024-02-07 14:28:08 +0600

    user profile done

--- src/app/dashboard/myBLog/page.js
+++ src/app/dashboard/myBLog/page.js
@@ -11,7 +11,7 @@ const page = () => {
     return (
         <div className='mt-20 min-h-screen'>
             <DashboardTitle subTitle='Your Posted Blogs' headerTitle='See your all blog'></DashboardTitle>
-            <div>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3'>
                 {
                     myBlog?.map(item => <BlogCard key={item?._id} item={item}></BlogCard>)
                 }
