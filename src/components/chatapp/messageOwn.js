382715c91e41a09fc15e96b8d6bcccdc326b46b1
shoptorshi <shoptorshiria78@gmail.com>
2024-03-06 21:46:11 +0600

    merged the analytic section

--- src/components/chatapp/messageOwn.js
+++ src/components/chatapp/messageOwn.js
@@ -1,21 +1,29 @@
 "use client"
 
+import useAuth from "@/app/hooks/useAuth";
+import useUsersData from "@/app/hooks/useUsersData";
 import { Avatar, Grid, Typography } from "@mui/material";
 import { format } from "date-fns";
 
 
 const MessageOwn = (message, own) => {
 
-    console.log(message)
+    const { user } = useAuth()
+    const [users] = useUsersData()
+    const friendId = message.message.sender;
+    const friendDetails = users.find(user => user._id === friendId)
+
+    // console.log(message.message.sender)
+
     return (
         <div >
-           
+
             {
                 message.own ?
                     <>
                         {/* own message */}
                         <Grid container sx={{ alignItems: "center", mt: 3 }}>
-                            <Grid item> <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" /></Grid>
+                            <Grid item> <Avatar alt="Travis Howard" src={user?.photoURL} /></Grid>
                             <Grid item>
                                 <Typography sx={{ ml: 1, width: 350, background: "#4f675b", color: "white", p: 2, borderRadius: 5 }} >{message.message.text}</Typography>
                             </Grid>
@@ -32,7 +40,7 @@ const MessageOwn = (message, own) => {
 
                                 <Typography sx={{ mr: 1, width: 350, background: "#eeeeee", color: "black", p: 2, borderRadius: 5 }} >{message.message.text}</Typography>
                             </Grid>
-                            <Grid item> <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" /></Grid>
+                            <Grid item> <Avatar alt="Travis Howard" src={friendDetails?.uPhoto} /></Grid>
 
                         </Grid>
                         {/* <Typography sx={{ textAlign: "end", mt: 1, fontSize: 14 }}>{format(message.createdAt)}</Typography> */}
